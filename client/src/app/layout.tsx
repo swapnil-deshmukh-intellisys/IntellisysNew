56aa7699db10977bd79517b7909217eaef8fcfca
ritesh-intellisys <ritesh.intellisys@gmail.com>
2026-03-02 15:29:36 +0530

    removed ad logo

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -24,10 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}
-
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fintellisys7711back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+      <body>{children}</body>
     </html>
   );
 }
